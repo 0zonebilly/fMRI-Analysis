1369aac6f13f04ee3144c27fd086e7f25b3a570d
Thomas Varley <tfv21@compute-image.localdomain>
2017-11-20 20:54:49 +0000

    Added PR maps

--- complexity-entropy/point_process_analysis.py
+++ complexity-entropy/point_process_analysis.py
@@ -3,6 +3,7 @@
 Author: Thomas Varley
 
 This implements point process analysis on a neural timeseries.
+2D and 3D implementations of a Poincare return map are included, for use on the processed timeseries. 
 
 Described by Tagliazucci et al.
 
@@ -11,6 +12,11 @@ Tagliazucchi et al., “Enhanced Repertoire of Brain Dynamical States during the
 """
 
 import numpy as np
+
+####################################################
+#Point Process Analysis
+####################################################
+
 
 def point_process(timeseries):
     from copy import copy
@@ -61,7 +67,96 @@ def ppa_interval(timeseries):
             counter += 1
             i += 1
     return np.mean(intervals)
-            
+
+####################################################
+#Poincare Return Maps
+####################################################
+
+#Returns the interval, in seconds, between one event and the next
+def interval(timeseries, TR):
+    length = len(timeseries)
+    intervals = []
+    i = 0
+    counter = 0
+    
+    while i < length:
+        if timeseries[i] == 1:
+            intervals.append(counter)
+            counter = 0
+            i += 1
+        elif timeseries[i] == 0:
+            counter += 1
+            i += 1
+    intervals = [TR*x for x in intervals]
+    return intervals
+
+#Creates a 2D Poincare return map of the binary timeseries
+def poincare_return_2D(timeseries):
+    import matplotlib as plt
+    x_axis = interval(timeseries)
+    y_axis = x_axis[1:]
+    x_axis = x_axis[:-1]
+    
+    plt.pyplot.scatter(x_axis, y_axis)
+
+#Creates a 3D Poincare return map of the binary timeseries
+def poincare_return_3D(timeseries):
+    from matplotlib import pyplot
+    from mpl_toolkits.mplot3d import Axes3D
+    
+    x_axis = interval(timeseries)
+    y_axis = x_axis[1:-1]
+    z_axis = x_axis[2:]
+    x_axis = x_axis[:-2]
+    
+    fig = pyplot.figure()
+    ax = Axes3D(fig)
+    
+    ax.scatter(x_axis, y_axis, z_axis)
+    pyplot.show()
+
+#Returns the interval, in seconds, between one event and the next
+def interval(timeseries, TR):
+    length = len(timeseries)
+    intervals = []
+    i = 0
+    counter = 0
+    
+    while i < length:
+        if timeseries[i] == 1:
+            intervals.append(counter)
+            counter = 0
+            i += 1
+        elif timeseries[i] == 0:
+            counter += 1
+            i += 1
+    intervals = [TR*x for x in intervals]
+    return intervals
+
+#Creates a 2D Poincare return map of the binary timeseries
+def poincare_return_2D(timeseries):
+    import matplotlib as plt
+    x_axis = interval(timeseries)
+    y_axis = x_axis[1:]
+    x_axis = x_axis[:-1]
+    
+    plt.pyplot.scatter(x_axis, y_axis)
+
+#Creates a 3D Poincare return map of the binary timeseries
+def poincare_return_3D(timeseries):
+    from matplotlib import pyplot
+    from mpl_toolkits.mplot3d import Axes3D
+    
+    x_axis = interval(timeseries)
+    y_axis = x_axis[1:-1]
+    z_axis = x_axis[2:]
+    x_axis = x_axis[:-2]
+    
+    fig = pyplot.figure()
+    ax = Axes3D(fig)
+    
+    ax.scatter(x_axis, y_axis, z_axis)
+    pyplot.show()
             
     
     
